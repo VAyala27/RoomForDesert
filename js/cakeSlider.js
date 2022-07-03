cfaf55c69a15015da16475e0a37fa907639c9938
Victor Ayala <victorayala1996@gmail.com>
2022-07-03 02:44:42 -0400

    remove some files, update html, js, and scss

---
+++ js/cakeSlider.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        slidesToShow: 3,
+        centerMode: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        infinite: true,
+        swipeToSlide: true,
+        arrows: false,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
--- js/index.js
+++ js/index.js
@@ -1,38 +1,10 @@
-$("carousel").carousel({
-  interval: 6000,
-  pause: "hover"
-});
-
-$(document).ready(function() {
-  $("#lightSlider").lightSlider({
-    item: 3,
-    controls: true,
-    prevText: "Previous",
-    nextText: "Next",
-    auto: true,
-    loop: true,
-    speed: 3000,
-    pause: 5000
-  });
-});
-$(function() {
-  $("#myCarousel").carousel({
-    interval: 5000
-  });
-  $("#myCarousel").on("slid.bs.carousel", function(e) {
-    if ($(".carousel-inner .item:last").hasClass("active")) {
-      $("#myCarousel").carousel("pause");
-    }
-    if ($(".carousel-inner .item:first").hasClass("active")) {
-      $("#myCarousel").carousel("cycle");
-    }
-  });
-});
-
 //Initalize and add the map
 function initMap() {
   //Your location
-  const loc = { lat: 42.361145, lng: -71.057083 };
+  const loc = {
+    lat: 42.361145,
+    lng: -71.057083
+  };
   //centered map on location
   const map = new google.maps.Map(document.querySelector(".map"), {
     zoom: 14,
@@ -44,12 +16,3 @@ function initMap() {
     map: map
   });
 }
-
-// // Sticky menu background Regular js
-// window.addEventListener("scroll", function() {
-//   if (window.scrollY > 150) {
-//     document.querySelector("#navbar").style.opacity = 0.7;
-//   } else {
-//     document.querySelector("#navbar").style.opacity = 1;
-//   }
-// });
